ee97dd4f7b61092c541a602a70294e714536f60f
Carlos Eduardo <suportebeloj@gmail.com>
2022-09-19 22:25:58 -0300

    basic app config

--- app.vue
+++ app.vue
@@ -1,20 +1,5 @@
-<script setup lang="ts">
-import { useAuthStore } from "@/storage/auth";
-const authStore = useAuthStore();
-
-watch(authStore, () => {
-  localStorage.setItem("token", authStore.token)
-})
-
-</script>
-
 <template>
-  <div>
-    <p>
-      store: {{ authStore.getToken }}
-    </p>
-    <p>
-      <input type="text" v-model="authStore.token">
-    </p>
-  </div>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
