6a7eaaf273b17fb329dad738532ecdc4c508a9c2
Carlos Eduardo <suportebeloj@gmail.com>
2022-09-21 14:38:47 -0300

    font size display added from themes

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,6 +16,14 @@ module.exports = {
         "switcher-black-bg-color": "#3c3c3c",
         "switcher-blue-contrast-color": "#68bad4"
       },
+      fontSize: {
+        "display-1": "5rem",
+        "display-2": "4.5rem",
+        "display-3": "4rem",
+        "display-4": "3.5rem",
+        "display-5": "3rem",
+        "display-6": "2.5rem",
+      }
     },
   },
   plugins: [],
