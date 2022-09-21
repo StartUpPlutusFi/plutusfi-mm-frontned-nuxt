7273bf76da7b00c428617109e89e45b5dbb28d61
Carlos Eduardo <suportebeloj@gmail.com>
2022-09-21 16:51:44 -0300

    component button

---
+++ components/ui_elements/Button.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+const props = defineProps(["text"])
+const emit = defineEmits(["roll"]);
+const callEmitRoll = () => {
+  emit("roll");
+};
+</script>
+<template>
+  <button @click="callEmitRoll()" v-text="props.text" class="btn btn-primary btn-expand"></button>
+</template>
