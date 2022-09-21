0423e836e8a6c9f38f62493dd437643c0162603f
Carlos Eduardo <suportebeloj@gmail.com>
2022-09-21 11:41:05 -0300

    placeholder

--- components/ui_elements/TextInput.vue
+++ components/ui_elements/TextInput.vue
@@ -1,5 +1,8 @@
+<script setup lang="ts">
+const placeholder: string = ""
+</script>
 <template>
-    <input type="text" class="input-text" placeholder="Username">
+    <input type="text" class="input-text" :placeholder="placeholder">
 </template>
 
 <style lang="scss" scoped>
