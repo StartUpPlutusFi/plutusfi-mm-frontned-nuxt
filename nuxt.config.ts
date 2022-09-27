9b504a029c13747b6834ced57ac4a1a2f2ebb1a4
Carlos Eduardo <suportebeloj@gmail.com>
2022-09-27 16:05:27 -0300

    css config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,5 +4,5 @@ import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   modules: ["@pinia/nuxt", "@nuxtjs/tailwindcss", "@nuxtjs/color-mode"],
-  css: ["~/assets/css/tailwind.css"]
+  css: ["~/assets/css/tailwind.css", "mosha-vue-toastify/dist/style.css"],
 });
