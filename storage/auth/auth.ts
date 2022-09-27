00deb0a2fdfd36354af8735575dd4444699d360f
Carlos Eduardo <suportebeloj@gmail.com>
2022-09-27 18:18:26 -0300

    AuthStore Pinia

---
+++ storage/auth/auth.ts
@@ -0,0 +1,22 @@
+import { defineStore } from "pinia";
+import { AuthCredentials } from "./interfaces";
+
+
+export const useAuthStore = defineStore("auth", {
+  state: () => {
+    return {
+      auth: null as AuthCredentials | null,
+    };
+  },
+  getters: {
+    getAccess: (state) => state.auth?.access,
+    getRefresh: (state) => state.auth?.refresh,
+  },
+
+  actions: {
+    setAuthCredentials(auth: AuthCredentials) {
+      this.auth = auth;
+    },
+  },
+  persist: true,
+});
