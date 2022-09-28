e814e8085f5a6e92ef9f964924e412eef62bcbda
Carlos Eduardo <suportebeloj@gmail.com>
2022-09-28 10:41:23 -0300

    login api

---
+++ server/auth/login.ts
@@ -0,0 +1,25 @@
+import { AxiosError } from "axios";
+import { createToast } from "mosha-vue-toastify";
+import { AuthCredentials } from "~~/storage/auth/interfaces";
+
+import API from "../api";
+
+export interface Credentials {
+  username: string;
+  password: string;
+}
+export const LoginAPI = async (
+  credentiais: FormData
+): Promise<{ data: AuthCredentials | null }> => {
+  const { data } = await API.post("/auth/api/token/login/", credentiais).catch(
+    (error: AxiosError) => {
+      let { detail }: string = error.response.data;
+      createToast(detail, {
+        position: "top-center",
+        type: "danger",
+      });
+    }
+  );
+
+  return data;
+};
