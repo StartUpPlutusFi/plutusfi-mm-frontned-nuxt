73c063aebabdc25f7561449c3cb4b95f039e789e
Carlos Eduardo <suportebeloj@gmail.com>
2022-09-28 15:46:06 -0300

    refactoring is-authenticated middleware

--- plugins/auth.ts
+++ plugins/auth.ts
@@ -1,17 +1,19 @@
+import { useLoader } from "./../storage/loader";
 import { useAuthStore } from "~~/storage/auth/auth";
 import { createToast } from "mosha-vue-toastify";
 
-
 export default defineNuxtPlugin(() => {
-  addRouteMiddleware("is-authenticated", (to) => {
-    const auth = useAuthStore()
-    if (to.meta.loginRequired && !auth.getAccess) {
-      createToast("Login is required to acess page.", {
-        type: "danger",
-        position: "top-center"
-      });
-      return navigateTo("/login")
-    }
+  addRouteMiddleware(
+    "is-authenticated",
+    (to) => {
+      const auth = useAuthStore();
+      if (to.meta.loginRequired && !auth.getAccess) {
+        createToast("Login is required to acess page.", {
+          type: "danger",
+          position: "top-center",
+        });
+        return navigateTo("/login");
+      }
     },
     {}
   ),
