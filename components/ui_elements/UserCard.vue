0130c2af4d734584d1074752f152a32576854ca8
Carlos Eduardo <suportebeloj@gmail.com>
2022-10-01 16:50:15 -0300

    logout function

--- components/ui_elements/UserCard.vue
+++ components/ui_elements/UserCard.vue
@@ -1,28 +1,54 @@
 <script setup lang="ts">
+import { useProfile } from "~~/storage/profile";
+import autoAnimate from "@formkit/auto-animate";
+import { useAuthStore } from "~~/storage/auth/auth";
+
 const showSettingsMenu = ref<boolean>(false);
+const dropdown = ref<HTMLDivElement>();
+const profile = useProfile();
+const auth = useAuthStore();
+const router = useRouter();
 
 const toggle = () => {
   showSettingsMenu.value = !showSettingsMenu.value;
 };
 
+const logout = () => {
+  profile.clear();
+  auth.clear();
+  navigateTo("/login");
+};
+
+onMounted(() => {
+  autoAnimate(dropdown.value);
+});
 </script>
 <template>
-  <div class="md:w-40 h-full flex relative">
+  <div class="md:w-40 h-full flex relative" ref="dropdown">
     <div class="px-3 text-right">
-      <p class="capitalize text-gray-300 hover:text-white">username</p>
+      <p class="capitalize text-gray-300 hover:text-white">
+        {{ profile.get?.user.username }}
+      </p>
       <span class="cursor-pointer text-sm text-gray-400" @click="toggle()"
         >settings</span
       >
     </div>
     <ui-elements-profile-picture />
     <div
-      :class="{ show: showSettingsMenu }"
-      class="absolute top-12 right-20 bg-gray-500 p-5 z-20 hidden opacity-0"
+      v-if="showSettingsMenu"
+      class="absolute top-12 right-24 bg-gray-500 z-20 p-1 block rounded-md"
     >
-      <ul>
-        <li>option</li>
-        <li>option</li>
-        <li>option</li>
+      <ul class="list-none text-gray-300">
+        <li class="list-link">Settings</li>
+        <li
+          class="list-link"
+          @click="
+            toggle();
+            logout();
+          "
+        >
+          Logout
+        </li>
       </ul>
     </div>
   </div>
