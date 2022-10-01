00b17baf3d4ac30a5b272e6829be672da2803565
Carlos Eduardo <suportebeloj@gmail.com>
2022-10-01 10:30:52 -0300

    api get profile info

---
+++ server/auth/profile.ts
@@ -0,0 +1,21 @@
+import { createToast } from "mosha-vue-toastify";
+import { AxiosError } from "axios";
+import { ProfileInfo } from "./../../storage/profile/interfaces";
+import { useAuthStore } from "~~/storage/auth/auth";
+import API from "../api";
+
+export const GetProfileInfo = async (): Promise<ProfileInfo | null> => {
+  const auth = useAuthStore();
+  
+  const { data } = await API.get("/auth/api/token/profile/", {
+    headers: {
+      Authorization: `Bearer ${auth.getAccess}`,
+    },
+  }).catch((error: AxiosError) => {
+    createToast(error, {
+      type: "danger",
+    });
+  });
+
+  return data;
+};
