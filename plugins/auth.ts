5e2d3930be308c72da109882081eb82ddc8f8812
Carlos Eduardo <suportebeloj@gmail.com>
2022-10-01 16:29:00 -0300

    clear data in case user are not authenticated

--- plugins/auth.ts
+++ plugins/auth.ts
@@ -1,17 +1,21 @@
 import { useLoader } from "./../storage/loader";
 import { useAuthStore } from "~~/storage/auth/auth";
 import { createToast } from "mosha-vue-toastify";
+import { useProfile } from "~~/storage/profile";
 
 export default defineNuxtPlugin(() => {
   addRouteMiddleware(
     "is-authenticated",
     (to) => {
       const auth = useAuthStore();
+      const profile = useProfile();
       if (to.meta.loginRequired && !auth.getAccess) {
         createToast("Login is required to acess page.", {
           type: "danger",
           position: "top-center",
         });
+        auth.clear()
+        profile.clear()
         return navigateTo("/login");
       }
     },
