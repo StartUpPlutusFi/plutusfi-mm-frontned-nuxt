1a1bc19b3dd2fab79050b1557338927ae4018f4f
Carlos Eduardo <suportebeloj@gmail.com>
2022-10-03 15:27:30 -0300

    alter defaul class layout from css

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts"></script>
 <template>
-    <div class="bg-plutusfi-login-bg-color min-h-screen">
+    <div class="bg-plutusfi-login-bg-color min-h-screen flex flex-col">
         <app-nav-bar />
         <slot />
     </div>
