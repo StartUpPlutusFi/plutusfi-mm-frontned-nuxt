45fb4a9328fc625f16fecf0ce39005b4246e36bd
Carlos Eduardo <suportebeloj@gmail.com>
2022-10-09 15:42:37 -0300

    coming soon  added into pages

--- pages/exchange.vue
+++ pages/exchange.vue
@@ -1,3 +1,6 @@
 <template>
-    
+    <div class="grow flex flex-col items-center justify-center text-white">
+        <img src="~/assets/svg/undraw_designer_re_5v95.svg" width="100" alt="working">
+        <h1 class="text-display-5">Coming soon</h1>
+    </div>
 </template>
