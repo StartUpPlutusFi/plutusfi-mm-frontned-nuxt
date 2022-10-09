08d91a91092f28fbceb1b44646b73aa5edf42884
Carlos Eduardo <suportebeloj@gmail.com>
2022-10-09 18:45:40 -0300

    Auto trade card

---
+++ components/AutoTraderCardBot/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="p-3 w-full bg-gray-700 rounded-md flex relative">
+    <div class="absolute right-3 top-2">
+        <div class="w-1 h-1 rounded-full bg-white mb-1"></div>
+        <div class="w-1 h-1 rounded-full bg-white mb-1"></div>
+        <div class="w-1 h-1 rounded-full bg-white"></div>
+    </div>
+    <div class="mr-2">
+      <img 
+      src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/46/Bitcoin.svg/2048px-Bitcoin.svg.png" 
+      alt="project-name-pic" width="180" height="180" />
+    </div>
+    <div>
+        <div>
+            <h4 class="font-semibold text-gray-100">project name</h4>
+            <span
+            class="text-sm text-gray-400"
+              >Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cupiditate
+              magnam quo itaque?</span
+            >
+        </div>
+        <div class="mt-2">
+            <button class="btn btn-primary">start</button>
+        </div>
+    </div>
+  </div>
+</template>
