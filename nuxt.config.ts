93cccf658498e183f0af89d0fe77e77f62cdda01
Carlos Eduardo <suportebeloj@gmail.com>
2022-10-09 18:44:22 -0300

    nuxt pwa module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,11 @@ import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  modules: ["@pinia/nuxt", "@nuxtjs/tailwindcss", "@nuxtjs/color-mode"],
+  modules: [
+    "@pinia/nuxt",
+    "@nuxtjs/tailwindcss",
+    "@nuxtjs/color-mode",
+    "@nuxtjs/pwa",
+  ],
   css: ["~/assets/css/tailwind.css", "mosha-vue-toastify/dist/style.css"],
 });
