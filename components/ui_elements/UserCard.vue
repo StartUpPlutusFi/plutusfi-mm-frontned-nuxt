0ab321e18a8572d07f42c9b9d5d2717168580fd7
Carlos Eduardo <suportebeloj@gmail.com>
2022-10-10 15:18:34 -0300

    func to delete session cookies

--- components/ui_elements/UserCard.vue
+++ components/ui_elements/UserCard.vue
@@ -13,9 +13,21 @@ const toggle = () => {
   showSettingsMenu.value = !showSettingsMenu.value;
 };
 
+function deleteAllCookies() {
+    var cookies = document.cookie.split(";");
+
+    for (var i = 0; i < cookies.length; i++) {
+        var cookie = cookies[i];
+        var eqPos = cookie.indexOf("=");
+        var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+        document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+    }
+}
+
 const logout = () => {
   profile.clear();
   auth.clear();
+  deleteAllCookies()
   navigateTo("/login");
 };
 
