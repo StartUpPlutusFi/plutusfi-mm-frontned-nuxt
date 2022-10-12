51d5db828e899f892ea30e05e6b3481dde9f3b8b
Carlos Eduardo <suportebeloj@gmail.com>
2022-10-12 09:16:57 -0300

    api card list

---
+++ components/ApiCredentialList/index.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="grow flex flex-col mt-5 overflow-x-auto">
+        <div class="w-full bg-gray-500">
+            <p>a</p>
+            <p>a</p>
+        </div>
+    </div>
+</template>
