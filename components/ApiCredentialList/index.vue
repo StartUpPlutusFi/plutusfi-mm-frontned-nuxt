5e2792b32cee64c52c9eb798db5184a0fc27776f
Carlos Eduardo <suportebeloj@gmail.com>
2022-11-07 11:46:18 -0300

    alter component to use crendential state and set animations from component

--- components/ApiCredentialList/index.vue
+++ components/ApiCredentialList/index.vue
@@ -15,22 +15,39 @@ onBeforeMount(async () => {
 </script>
 
 <template>
-    <div class="grow flex flex-col mt-5 overflow-x-auto">
-        <div class="w-full bg-gray-500 rounded-md" v-if="apiCredentialList.length > 0">
-            <transition-group name="list" tag="ul" class="list-none">
-                <li v-for="cred in apiCredentialList" :key="cred">
-                    <div class="p-2 flex justify-between">
-                        <div>
-                            {{ cred.description }}
-                            <p>Default: {{ cred.default ? "yes": "no" }}</p>
-                        </div>
-                        <button class="btn">X</button>
-                    </div>
-                </li>
-            </transition-group>
-        </div>
-        <div v-else class="">
-            <p class="text-center">No credentials registered.</p>
-        </div>
+  <div class="grow flex flex-col mt-5 overflow-x-auto">
+    <div
+        class="w-full bg-gray-500"
+        v-if="credentials.credentialList.length > 0"
+    >
+      <transition-group name="list" tag="ul" class="list-none">
+        <li v-for="(cred, index) in credentials.credentialList" :key="cred">
+          <div class="p-2 flex justify-between">
+            <div>
+              {{ cred.description }}
+              <p>Default: {{ cred.default ? "yes" : "no" }}</p>
+            </div>
+            <button class="btn" @click="deleteCredential(index)">X</button>
+          </div>
+        </li>
+      </transition-group>
     </div>
+    <div v-else class="">
+      <p class="text-center">No credentials registered.</p>
+    </div>
+  </div>
 </template>
+
+<style lang="scss" scoped>
+.list-enter-active,
+.list-leave-active {
+  transition: all 0.5s ease;
+}
+
+.list-enter-from,
+.list-leave-to {
+  opacity: 0;
+  transform: translateX(30px);
+}
+
+</style>
