63a519c9225a5ffbdf24b478823378449189e686
Carlos Eduardo <suportebeloj@gmail.com>
2022-11-07 18:55:48 -0300

    Auto Trader Grid component

---
+++ components/AutoTradeGrid/index.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import {AutoTraderCardBot} from "#components";
+import {useAutoTrader} from "~/storage/autotrader/auto-trader-store";
+import {onBeforeMount} from "#imports";
+
+const autoTrader = useAutoTrader()
+
+onBeforeMount(async () => {
+  await autoTrader.LoadTraders()
+  console.log(autoTrader.getTraderList)
+})
+</script>
+
+<template>
+  <div class="grid-auto-trader">
+    <auto-trader-card-bot
+        v-for="trader in autoTrader.getTraderList"
+        :name="trader.name"
+        :description="trader.description"
+    />
+  </div>
+</template>
+
+<style scoped>
+
+</style>
