bd6e3a03cbf91bcbad6ea3938dcbc4b6b06f709f
Carlos Eduardo <suportebeloj@gmail.com>
2022-11-07 19:00:25 -0300

    Auto trader config interface

---
+++ server/autotrader/interfaces.ts
@@ -0,0 +1,10 @@
+export interface AutoTraderConfig {
+    name: string
+    description: string
+    api_key_id: number,
+    user_id: number,
+    pair_token: string,
+    user_ref_price: number,
+    trade_candle: number,
+    trade_amount: number
+}
