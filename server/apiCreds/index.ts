ac4890ad8f1541e20cb43544942b1cd3f32a517c
Carlos Eduardo <suportebeloj@gmail.com>
2022-11-07 09:22:17 -0300

    api integration LoadExchange

--- server/apiCreds/index.ts
+++ server/apiCreds/index.ts
@@ -1,6 +1,6 @@
 import API from "../api";
-import { useAuthStore } from "./../../storage/auth/auth";
-import { ApiDetail, CredentialForm } from "./types";
+import { ApiDetail, CredentialForm, Exchange } from "./types";
+import {useAuthStore} from "~/storage/auth/auth";
 
 export const CreataApiCredentials = async (
   body: CredentialForm
@@ -16,6 +16,18 @@ export const CreataApiCredentials = async (
   });
 
   return data;
+};
+
+export const LoadExchangeList = async (): Promise<Exchange[]> => {
+  const auth = useAuthStore();
+
+  const { data } = await API.get("/exchange/list", {
+    headers: {
+      Authorization: `Bearer ${auth.getAccess}`,
+    },
+  });
+
+  return data
 };
 
 export const LoadApiCredentialList = async (): Promise<ApiDetail[]> => {
