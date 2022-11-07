9430d1c9dcf51714731ccb0fee8b7833330b6cab
Carlos Eduardo <suportebeloj@gmail.com>
2022-11-07 19:07:43 -0300

    import component, usevue to close option menu and import icon

--- components/AutoTraderCardBot/index.vue
+++ components/AutoTraderCardBot/index.vue
@@ -1,22 +1,46 @@
+<script setup lang="ts">
+import {onClickOutside} from '@vueuse/core'
+import {ref} from "#imports";
+import { Icon } from "@iconify/vue";
+
+const props = defineProps({
+  name: {type: String, required: true},
+  description: {type: String, required: true}
+})
+
+const showOptions = ref<boolean>(false)
+const optionRef = ref<HTMLDivElement>()
+
+const CloseOptionsMenu = () => {
+  showOptions.value = false
+}
+
+onClickOutside(optionRef, CloseOptionsMenu)
+</script>
 <template>
   <div class="p-3 w-full bg-gray-700 rounded-md flex relative">
-    <div class="absolute right-3 top-2">
-        <div class="w-1 h-1 rounded-full bg-white mb-1"></div>
-        <div class="w-1 h-1 rounded-full bg-white mb-1"></div>
-        <div class="w-1 h-1 rounded-full bg-white"></div>
+    <div @click="showOptions = !showOptions" class="absolute right-1 text-gray-400  top-2 hover:text-white hover:cursor-pointer w-5">
+      <Icon icon="charm:menu-kebab" />
+    </div>
+    <div v-if="showOptions" ref="optionRef" class="min-h-fit absolute right-3 top-7 bg-gray-300 rounded">
+      <ul>
+        <li class="hover:bg-gray-500 hover:text-white cursor-pointer px-2 rounded-t">Info</li>
+        <li>Settings</li>
+        <li>Delete</li>
+      </ul>
     </div>
     <div class="mr-2">
-      <img 
+      <img
+          class="w-20"
       src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/46/Bitcoin.svg/2048px-Bitcoin.svg.png" 
       alt="project-name-pic" width="180" height="180" />
     </div>
     <div>
         <div>
-            <h4 class="font-semibold text-gray-100">project name</h4>
+            <h4 class="font-semibold text-gray-100">{{ props.name }}</h4>
             <span
             class="text-sm text-gray-400"
-              >Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cupiditate
-              magnam quo itaque?</span
+              >{{ props.description }}</span
             >
         </div>
         <div class="mt-2">
