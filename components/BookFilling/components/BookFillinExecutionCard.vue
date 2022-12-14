ba40b653fe792fa13c243f9a59f82d12ff09a67e
Carlos Eduardo <suportebeloj@gmail.com>
2022-12-14 16:08:59 -0300

    execution card, used to show execution information on grid

---
+++ components/BookFilling/components/BookFillinExecutionCard.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import {ResponseCreateExecutionBookfiller} from "~/storage/bookfiller/interfaces";
+import {convertStringToDate} from "~/utils/date_utils"
+
+const props = defineProps({
+  index: {Type: Number, required: true},
+  data: {Type: Object as unknown as ResponseCreateExecutionBookfiller, required: true}
+})
+
+const emit = defineEmits(["detail"])
+
+
+</script>
+
+<template>
+  <div class="text-gray-200 border border-gray-500 hover:border-gray-900 bg-gray-700 hover:text-white hover:bg-gray-800 transition-colors hover: flex flex-col p-2 justify-between rounded">
+    <div>
+      <h4>{{ props.data.name }}</h4>
+    </div>
+    <p>
+      <small>Execution at: {{ convertStringToDate(props.data.created_at).toDateString() }}</small>
+    </p>
+    <div class="flex justify-end">
+      <small
+          @click="emit('detail', props.index)"
+          class="text-gray-400 hover:text-gray-50 cursor-pointer">detail</small>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
